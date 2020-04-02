ff1c1d0c01bb443eec6ddb27ea001773ae76b511
cnugroho11 <cnugroho211@gmail.com>
2020-04-02 11:13:53 +0700

    progress pagi

--- tr_asd/main.c
+++ tr_asd/main.c
@@ -3,6 +3,15 @@
 #include <string.h>
 #include <windows.h>
 
+int ii = 0;
+
+struct kegiatan
+{
+    char logg[100];
+};
+
+struct kegiatan keg[100];
+
 char usr[25],pass[25],ch;
 
 void get_time()
@@ -26,13 +35,13 @@ void quit_program()
     for(int i = 3; i >= 0; i--)
     {
         system("cls");
-        gotoxy(45,11);printf("====================================\n");
-        gotoxy(45,12);printf("++++Program akan keluar dalam %d++++\n", i);
-        gotoxy(45,13);printf("====================================\n");
+        gotoxy(40,11);printf("====================================\n");
+        gotoxy(40,12);printf("++++Program akan keluar dalam %d++++\n", i);
+        gotoxy(40,13);printf("====================================\n");
         Sleep(1000);
     }
 
-    gotoxy(45,15);printf("Terimakasih!!\n");
+    gotoxy(40,15);printf("Terimakasih!!\n");
     Sleep(1000);
     exit(0);
 }
@@ -40,14 +49,14 @@ void quit_program()
 void login()
 {
     system("cls");
-    gotoxy(45,11);printf("Selamat Datang di Kamus Ala Kadarnya\n");
-    gotoxy(45,12);printf("                                    \n");
-    gotoxy(45,13);printf("====================================\n");
-    gotoxy(45,16);printf("====================================\n");
-    gotoxy(45,17);printf("++++++++++++++++++++++++++++++++++++\n");
-    gotoxy(51,18);get_time();
-    gotoxy(45,14);printf("Username     : ");scanf("%s", usr);
-    gotoxy(45,15);printf("Pass (max 8) : ");
+    gotoxy(40,11);printf("Selamat Datang di Kamus Ala Kadarnya\n");
+    gotoxy(40,12);printf("                                    \n");
+    gotoxy(40,13);printf("====================================\n");
+    gotoxy(40,16);printf("====================================\n");
+    gotoxy(40,17);printf("++++++++++++++++++++++++++++++++++++\n");
+    gotoxy(45,18);get_time();
+    gotoxy(40,14);printf("Username   : ");scanf("%s", usr);
+    gotoxy(40,15);printf("Pass       : ");
     int i = 0;
     while(1)
     {
@@ -57,7 +66,6 @@ void login()
             pass[i] = ch;
             ch = '*' ;
             printf("%c",ch);
-            //i++;
         }
         else
         {
@@ -75,18 +83,21 @@ void login_check()
     if(c1 == 0 && c2 == 0)
     {
         system("cls");
-        gotoxy(45,11);printf("====================================\n");
-        gotoxy(45,12);printf("==========Berhasil Login!!==========\n");
-        gotoxy(45,13);printf("====================================\n");
+        gotoxy(40,11);printf("====================================\n");
+        gotoxy(40,12);printf("==========Berhasil Login!!==========\n");
+        gotoxy(40,13);printf("====================================\n");
+        strcpy(keg[ii].logg, "Admin Telah Login");
+        ii++;
+        menu();
     }
     else
     {
         system("cls");
-        gotoxy(45,11);printf("====================================\n");
-        gotoxy(45,12);printf("!!!!Password atau Usename salah!!!!!\n");
-        gotoxy(45,13);printf("====================================\n");
-        gotoxy(45,14);printf("Mau coba login lagi? (y/n) \n");
-        gotoxy(45,15);scanf("\n%c", &pil);
+        gotoxy(40,11);printf("====================================\n");
+        gotoxy(40,12);printf("!!!!Password atau Usename salah!!!!!\n");
+        gotoxy(40,13);printf("====================================\n");
+        gotoxy(40,14);printf("Mau coba login lagi? (y/n) \n");
+        gotoxy(40,15);scanf("\n%c", &pil);
         if(pil == 'y' || pil == 'Y') login();
         else
         {
@@ -97,20 +108,22 @@ void login_check()
 
 void menu()
 {
+    int cek;
     system("cls");
-    gotoxy(45,11);printf("Selamat Datang di Kamus Ala Kadarnya\n");
-    gotoxy(45,12);printf("                                    \n");
-    gotoxy(45,13);printf("====================================\n");
-    gotoxy(45,14);printf("==========Silakan Pilih Menu========\n");
-    gotoxy(45,15);printf("1. Membuat Database Kamus\n");
-    gotoxy(45,16);printf("2. Menampilkan Isi Database Kamus\n");
-    gotoxy(45,17);printf("3. Menambah Data Kamus\n");
-    gotoxy(45,18);printf("4. Menghapus Database Kamus\n");
-    gotoxy(45,19);printf("5. Mengedit Database Kamus\n");
-    gotoxy(45,20);printf("6. Mencari Database Kamus\n");
-    gotoxy(45,21);printf("7. Mengurutkan Data pada Database\n");
-    gotoxy(45,22);printf("8. Log Aktivitas\n");
-    gotoxy(45,23);printf("9. Keluar dari Program\n");
+    gotoxy(40,9);printf("==Selamat Datang di Kamus Ala Kadarnya==\n");
+    gotoxy(40,10);printf("\n");
+    gotoxy(40,11);printf("========================================\n");
+    gotoxy(40,12);printf("++==========Silakan Pilih Menu========++\n");
+    gotoxy(40,13);printf("| 1. Membuat Database Kamus            |\n");
+    gotoxy(40,14);printf("| 2. Menampilkan Isi Database Kamus    |\n");
+    gotoxy(40,15);printf("| 3. Menambah Data Kamus               |\n");
+    gotoxy(40,16);printf("| 4. Menghapus Database Kamus          |\n");
+    gotoxy(40,17);printf("| 5. Mengedit Database Kamus           |\n");
+    gotoxy(40,18);printf("| 6. Mencari Database Kamus            |\n");
+    gotoxy(40,19);printf("| 7. Mengurutkan Data pada Database    |\n");
+    gotoxy(40,20);printf("| 8. Log Aktivitas                     |\n");
+    gotoxy(40,21);printf("| 9. Keluar dari Program               |\n");
+    gotoxy(40,23);printf("+______________________________________+\n");
 }
 
 struct tnode
@@ -150,41 +163,69 @@ void insert_data(char ind[25], char ja[25], char ing[25], char ga[25], char ar[2
 
 void print_data()
 {
-    printf("Bahasa Indonesia\t\tBahasa Jawa\t\tBahasa Inggris\t\tBahasa Gaul\t\tArtinya\n");
+    system("cls");
+    gotoxy(10,8);printf("=======================Berikut adalah data yang anda masukan===========================");
+    gotoxy(10,9);printf("=======================================================================================");
+    gotoxy(10,10);printf("Bahasa Indonesia");gotoxy(30,10);printf("Bahasa Jawa");gotoxy(50,10);printf("Bahasa Inggris");gotoxy(70,10);printf("Bahasa Gaul");gotoxy(90,10);printf("Artinya");
+    gotoxy(10,11);printf("+-------------------------------------------------------------------------------------+");
+    int i = 0;
     while (curr != NULL)
     {
-        printf("%s", curr -> indo);
-        printf("%s", curr -> jawa);
-        printf("%s", curr -> inggris);
-        printf("%s", curr -> gaul);
-        printf("%s", curr -> arti);
+        gotoxy(10,13+i);printf("%s ", curr -> indo);
+        gotoxy(30,13+i);printf("%s ", curr -> jawa);
+        gotoxy(50,13+i);printf("%s ", curr -> inggris);
+        gotoxy(70,13+i);printf("%s ", curr -> gaul);
+        gotoxy(90,13+i);printf("%s ", curr -> arti);
         curr = curr -> next;
         printf("\n");
+        i++;
     }
 }
-
 void input_data()
 {
+    void header()
+    {
+        gotoxy(40,4);printf("====================================\n");
+        gotoxy(40,5);printf("        Menu Input Database\n");
+        gotoxy(40,6);printf("++++++++++++++++++++++++++++++++++++\n");
+        gotoxy(40,8);printf("====================================\n");
+    }
     int tc;
+    system("cls");
+    header();
     char ind[25], ja[25], ing[25], ga[25], ar[25];
-    printf("Masukan banyak data : ");scanf("%d", &tc);
+    gotoxy(40,9);printf("Masukan banyak data : ");
+    gotoxy(63,9);scanf("%d", &tc);
     for(int i = 0; i < tc; i++)
     {
-        printf("Masukan data ke - %d\n", i+1);
-        printf("Bahasa Indonesia : ");scanf("%s", ind);
-        printf("Bahasa Inggris : ");scanf("%s", ja);
-        printf("Bahasa Jawa : ");scanf("%s", ing);
-        printf("Bahasa Gaul : ");scanf("%s", ga);
-        printf("Bahasa Arti : ");scanf("%s", ar);
+        system("cls");
+        header();
+        gotoxy(40,10);printf(":: Masukan data ke - %d\n", i+1);
+        gotoxy(40,11);printf(":: Bahasa Indonesia : ");scanf("%s", ind);
+        gotoxy(40,12);printf(":: Bahasa Inggris : ");scanf("%s", ja);
+        gotoxy(40,13);printf(":: Bahasa Jawa : ");scanf("%s", ing);
+        gotoxy(40,14);printf(":: Bahasa Gaul : ");scanf("%s", ga);
+        gotoxy(40,15);printf(":: Bahasa Arti : ");scanf("%s", ar);
         insert_data(ind, ja, ing, ga, ar);
     }
+    print_data();
+    strcpy(keg[ii].logg, "Admin Telah Memasukan Data");
+    ii++;
 }
 
-
-int main()
+void main()
 {
+    int tc;
     login();
-    input_data();
-    print_data();
-    return 0;
+    gotoxy(40,22);printf("| Masukan pilihan anda :               |\n");
+    gotoxy(65,22);scanf("%d", &tc);
+    switch(tc)
+    {
+    case 1:
+        input_data();
+        break;
+    case 2:
+        print_data();
+        break;
+    }
 }
